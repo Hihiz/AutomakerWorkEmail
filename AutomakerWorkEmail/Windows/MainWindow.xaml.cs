0a1d8cb0ef0bf3c2ea81e40db68affebf2fa3c9b
Hihiz <public@mail.ru>
2023-03-02 00:10:38 +0300

    Сделать вывод данных из таблицы ClientOrder(отслеживание заказов клиентов) с внешними ключами и определить разметку xaml

--- AutomakerWorkEmail/Windows/MainWindow.xaml.cs
+++ AutomakerWorkEmail/Windows/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using AutomakerWorkEmail.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Windows;
 
 namespace AutomakerWorkEmail.Windows
 {
@@ -7,6 +10,22 @@ namespace AutomakerWorkEmail.Windows
         public MainWindow()
         {
             InitializeComponent();
+
+            using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
+            {
+                gridClientOrder.ItemsSource = db.ClientOrders.Include(s => s.Service).Include(c => c.Client).ToList();
+            }
+        }
+
+        private void ButtonHello_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
