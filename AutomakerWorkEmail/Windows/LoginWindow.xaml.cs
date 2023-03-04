82db05e7424a0734f4293c6f2718f97f945ebf10
Hihiz <public@mail.ru>
2023-03-04 23:15:58 +0300

    Создать окно для авторизации

--- AutomakerWorkEmail/Windows/LoginWindow.xaml.cs
+++ AutomakerWorkEmail/Windows/LoginWindow.xaml.cs
@@ -1,27 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AutomakerWorkEmail.Infrastructure;
+using AutomakerWorkEmail.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace AutomakerWorkEmail.Windows
 {
-    /// <summary>
-    /// Логика взаимодействия для LoginWindow.xaml
-    /// </summary>
     public partial class LoginWindow : Window
     {
+        bool verify = true;
+        int verifyCheck = 0;
+
         public LoginWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
+            {
+                if (panelCaptcha.Visibility == Visibility.Visible)
+                {
+                    if (textBlockCaptcha.Text == textBoxCaptcha.Text)
+                        verify = true;
+                }
+
+                Worker worker = db.Workers.Where(w => w.Login == textBoxLogin.Text && w.Password == passwordBox.Password).Include(r => r.Role).FirstOrDefault();
+
+                if (worker != null)
+                {
+                    new TrackingClientOrderWindow(worker).Show();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Логин или пароль введен не верно!", "Ошибка");
+
+                    verifyCheck += 1;
+
+                    panelCaptcha.Visibility = Visibility.Visible;
+                    textBlockCaptcha.Text = CaptchaBuilder.Refresh();
+                    verify = false;
+
+                    if (verifyCheck > 1)
+                    {
+                        DisableButtonAsync();
+                        textBlockCaptcha.Text = CaptchaBuilder.Refresh();
+                    }
+                }
+            }
+        }
+
+        private async void DisableButtonAsync()
+        {
+            buttonLogin.IsEnabled = false;
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            buttonLogin.IsEnabled = true;
         }
     }
 }
