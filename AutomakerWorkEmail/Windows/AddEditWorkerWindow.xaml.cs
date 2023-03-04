1da654949ba1743c8d9818086ff4315d5d18e217
Hihiz <public@mail.ru>
2023-03-04 23:08:54 +0300

    Сделать окно для просмотра, редактирования, добавления работников

---
+++ AutomakerWorkEmail/Windows/AddEditWorkerWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using AutomakerWorkEmail.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AutomakerWorkEmail.Windows
+{
+    public partial class AddEditWorkerWindow : Window
+    {
+        Worker currentWorker;
+
+        public AddEditWorkerWindow(Worker worker)
+        {
+            InitializeComponent();
+
+            using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
+            {
+                comboBoxRole.ItemsSource = db.Roles.ToList();
+            }
+
+            if (worker != null)
+            {
+                currentWorker = worker;
+                Title = $"Редактирование работника: {worker.FirstName} {worker.LastName} {worker.Patronymic} | {worker.Role.Name}";
+                DataContext = currentWorker;
+            }
+            else
+            {
+                Title = "Добавление работника";
+                panelId.Visibility = Visibility.Collapsed;
+
+              
+            }
+        }
+
+        private void ButtonSave_Click(object sender, RoutedEventArgs e)
+        {
+            using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
+            {
+                if (currentWorker == null)
+                {
+                    Worker worker = new Worker()
+                    {
+                        FirstName = textBoxFirstName.Text,
+                        LastName = textBoxLastName.Text,
+                        Patronymic = textBoxPatronymic.Text,
+                        Login = textBoxLogin.Text,
+                        Password = textBoxPassword.Text,
+                        RoleId = ((Role)comboBoxRole.SelectedItem).Id
+                    };
+
+                    try
+                    {
+                        db.Workers.Add(worker);
+                        db.SaveChanges();
+                        MessageBox.Show("Работник добавлен", "Успешно");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        db.Workers.Update(currentWorker);
+                        db.SaveChanges();
+                        MessageBox.Show("Работник обновлен", "Успешно");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+
+                WorkerWindow workerWindow = Application.Current.Windows.OfType<WorkerWindow>().FirstOrDefault();
+                (workerWindow.FindName("gridWorker") as DataGrid).ItemsSource = db.Workers.Include(r => r.Role).ToList();
+                (workerWindow.FindName("textBlockCountWorker") as TextBlock).Text = $"Количество: {db.Workers.Count()}";
+            }
+
+        }
+    }
+}
---
+++ AutomakerWorkEmail/Windows/WorkerWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using AutomakerWorkEmail.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Windows;
+
+namespace AutomakerWorkEmail.Windows
+{
+    public partial class WorkerWindow : Window
+    {
+        Worker currentWorker;
+
+        public WorkerWindow(Worker worker)
+        {
+            InitializeComponent();
+
+            using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
+            {
+                gridWorker.ItemsSource = db.Workers.Include(r => r.Role).ToList();
+                textBlockCountWorker.Text = $"Количество: {db.Workers.Count()}";
+            }
+
+            currentWorker = worker;
+
+            textBlockStatusWorker.Text = $"Вы вошли как: {worker.FirstName} {worker.LastName} {worker.Patronymic} | {worker.Role.Name}";
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MenuItemExitAdmin_Click(object sender, RoutedEventArgs e)
+        {
+            new LoginWindow().Show();
+
+            Close();
+        }
+
+        private void EditWorker_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            Worker currentWorker = gridWorker.SelectedItem as Worker;
+
+            new AddEditWorkerWindow(currentWorker).ShowDialog();
+        }
+
+        private void ButtonAddWorker_Click(object sender, RoutedEventArgs e)
+        {
+            new AddEditWorkerWindow(null).ShowDialog();
+        }
+
+        private void ButtonEditWorker_Click(object sender, RoutedEventArgs e)
+        {
+            Worker currentWorker = gridWorker.SelectedItem as Worker;
+
+            new AddEditWorkerWindow(currentWorker).ShowDialog();
+        }
+
+        private void ButtonDeleteWorker_Click(object sender, RoutedEventArgs e)
+        {
+            using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
+            {
+                Worker currentWorker = gridWorker.SelectedItem as Worker;
+
+                if (MessageBox.Show($"Вы точно хотите удалить {currentWorker.FirstName} {currentWorker.LastName}" +
+                    $" {currentWorker.Patronymic} | {currentWorker.Role.Name}", "Внимание",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    db.Workers.Remove(currentWorker);
+                    db.SaveChanges();
+                    MessageBox.Show($"Работник: {currentWorker.FirstName} {currentWorker.LastName} {currentWorker.Patronymic} удален");
+                }
+
+                textBlockCountWorker.Text = $"Количество: {db.Workers.Count()}";
+                gridWorker.ItemsSource = db.Workers.Include(r => r.Role).ToList();
+            }
+        }
+
+        private void MenuItemClientOrder_Click(object sender, RoutedEventArgs e)
+        {
+            new TrackingClientOrderWindow(currentWorker).Show();
+            Close();
+        }
+
+        private void MenuItemService_Click(object sender, RoutedEventArgs e)
+        {
+            new ServiceWindow(currentWorker).Show();
+            Close();
+        }
+
+        private void ButtonExitRole_Click(object sender, RoutedEventArgs e)
+        {
+            new LoginWindow().Show();
+            Close();
+
+        }
+
+        private void SearchFio_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
