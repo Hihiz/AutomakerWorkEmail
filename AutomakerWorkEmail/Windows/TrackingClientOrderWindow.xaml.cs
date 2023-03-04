88fdb27ac56ace8ec18024eb7f41c6db0d6ee87c
Hihiz <public@mail.ru>
2023-03-04 23:12:34 +0300

    Изменить menuItem, добавить кнопки, проверку на роль администратора

--- AutomakerWorkEmail/Windows/TrackingClientOrderWindow.xaml.cs
+++ AutomakerWorkEmail/Windows/TrackingClientOrderWindow.xaml.cs
@@ -7,20 +7,26 @@ namespace AutomakerWorkEmail.Windows
 {
     public partial class TrackingClientOrderWindow : Window
     {
-        public TrackingClientOrderWindow()
+        Worker currentWorker;
+
+        public TrackingClientOrderWindow(Worker worker)
         {
             InitializeComponent();
+
+            currentWorker = worker;
 
             using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
             {
                 gridClientOrder.ItemsSource = db.ClientOrders.Include(s => s.Service).Include(c => c.Client).ToList();
+                textBlockCountClientOrder.Text = $"Количество: {db.ClientOrders.Count()}";
+
+                if (currentWorker.RoleId == 1)
+                    menuItemAdmin.Visibility = Visibility.Visible;
+                else
+                    menuItemAdmin.Visibility = Visibility.Collapsed;
             }
-        }
 
-        private void ButtonHello_Click(object sender, RoutedEventArgs e)
-        {
-
-
+            textBlockStatusWorker.Text = $"Вы вошли как: {worker.FirstName} {worker.LastName} {worker.Patronymic} | {worker.Role.Name}";
         }
 
         private void MenuItemExit_Click(object sender, RoutedEventArgs e)
@@ -30,7 +36,54 @@ namespace AutomakerWorkEmail.Windows
 
         private void ButtonCloseOrder_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("На кнопку сделать закрытие заказа(удаление) с условие что работник введет код получения клиента");
+            ClientOrder currentClientOrder = gridClientOrder.SelectedItem as ClientOrder;
+
+            new CodeClientWindow(currentClientOrder).ShowDialog();
+
+            using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
+            {
+                gridClientOrder.ItemsSource = db.ClientOrders.Include(s => s.Service).Include(c => c.Client).ToList();
+                textBlockCountClientOrder.Text = $"Количество: {db.ClientOrders.Count()}";
+            }
+        }
+
+        private void EditClientOrder_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (currentWorker.RoleId == 1)
+            {
+                ClientOrder currentClientOrder = gridClientOrder.SelectedItem as ClientOrder;
+
+                new AddEditClientOrderWindow(currentClientOrder).ShowDialog();
+            }
+
+            using (AutomakerWorkEmailContext db = new AutomakerWorkEmailContext())
+            {
+                gridClientOrder.ItemsSource = db.ClientOrders.Include(s => s.Service).Include(c => c.Client).ToList();
+                textBlockCountClientOrder.Text = $"Количество: {db.ClientOrders.Count()}";
+            }
+        }
+
+        private void ButtonAddOrder_Click(object sender, RoutedEventArgs e)
+        {
+            new AddEditClientOrderWindow(null).ShowDialog();
+        }
+
+        private void MenuItemDataWorker_Click(object sender, RoutedEventArgs e)
+        {
+            new WorkerWindow(currentWorker).Show();
+            Close();
+        }
+
+        private void MenuItemServiceWindow_Click(object sender, RoutedEventArgs e)
+        {
+            new ServiceWindow(currentWorker).Show();
+            Close();
+        }
+
+        private void ButtonExitRole_Click(object sender, RoutedEventArgs e)
+        {
+            new LoginWindow().Show();
+            Close();
         }
     }
 }
